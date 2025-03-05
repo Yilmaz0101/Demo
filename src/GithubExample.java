f26d93c47274497aa58aaa1061fd3787340b02a7
Selah <selahattiny2000@gmail.com>
2025-03-05 11:49:15 -0500

    i modified some code

---
+++ src/GithubExample.java
@@ -0,0 +1,7 @@
+public class GithubExample {
+    public static void main(String[] args) {
+        System.out.println("I am learning git / github");
+
+        System.out.println("i am modifying my code");
+    }
+}
